136b4fb6331e13599581e094b73e2a78c33a3020
Yasminabdallah <yasminabdalllahgaberm@gmail.com>
2018-07-27 00:48:25 +0200

    edit on users migrations

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -6,17 +6,50 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 router.post('/login',function(req,res,next){
+  var email= req.body.email;
+  var password = req.body.password;
+  connection.query('SELECT * FROM users WHERE email = ?',[email], function (error, results, fields) {
+  if (error) {
+    // console.log("error ocurred",error);
+    res.send({
+      "code":400,
+      "failed":"error ocurred"
+    })
+  }else{
+    // console.log('The solution is: ', results);
+    if(results.length >0){
+      if(results[0].password == password){
+        res.send({
+          "code":200,
+          "success":"login sucessfull"
+            });
+      }
+      else{
+        res.send({
+          "code":204,
+          "success":"Email and password does not match"
+            });
+      }
+    }
+    else{
+      res.send({
+        "code":204,
+        "success":"Email does not exits"
+          });
+    }
+  }
+  });
 
 });
 router.post('/register',function(req,res,next){
   // console.log("req",req.body);
-  var today = new Date();
+
   var users={
-    "name":req.body.first_name,
-    "email":req.body.email,
+    "name":req.body.name,
     "password":req.body.password,
-    "created":today,
-    "modified":today
+    "email":req.body.email,
+    "socketid":req.body.socketid
+    
   }
   connection.query('INSERT INTO users SET ?',users, function (error, results, fields) {
   if (error) {
