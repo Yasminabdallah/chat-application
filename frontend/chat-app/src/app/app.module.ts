32f63cf57527ce258aadb7c5161c11da55dfd93d
Yasminabdallah <yasminabdalllahgaberm@gmail.com>
2018-07-27 02:20:07 +0200

    add login/register component and api service

--- frontend/chat-app/src/app/app.module.ts
+++ frontend/chat-app/src/app/app.module.ts
@@ -1,16 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
+import { NgModule, OnInit } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule} from '@angular/forms'
+import { AlertsModule } from 'angular-alert-module';
+import { ApiService } from './api.service';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
+const frontEndRoutes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+
+]
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      frontEndRoutes,
+       {enableTracing: true }
+    ),
+    HttpClientModule,
+    FormsModule,
+    AlertsModule.forRoot(),
+    
   ],
-  providers: [],
+  providers: [ApiService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
